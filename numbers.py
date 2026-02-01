705083090d4b8436716ba96139a6524b736e022b
ivalchanka <i.valchenk@hotmail.com>
2026-02-01 12:39:12 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -20,6 +20,13 @@ printTypeInfo(result2)
 
 # Example 3 (casting)
 result3 = 4 ** 4.0 # float
-result3Integer = int(result3) # It's a class not a method
+result3Integer1 = int(result3) # It's a class not a method
+result3Integer2 = int('100')
 
-printTypeInfo(result3Integer)
+printTypeInfo(result3Integer1)
+
+# Example 4 (round)
+result4 = 14 / 3
+result4Round = round(14 / 3, 2)
+
+print('Result:', result4, ' Round result:', result4Round)
