d45c4eb8e0a25e23310e44a1c468b505736f68a5
Ilya Valchanka <i.valchenk@hotmail.com>
2026-02-03 13:44:04 +0300

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,9 @@
 # Slicing
-listOfIntegers = [1, 2, 3, 4, 5]
+listOfIntegers = [1, 2, 3, 4, 5, 7, 8]
 print(listOfIntegers[:2]) # 1, 2
+
+# The 3rd parameter is a step size
+# 1
+# 1 + 2 = 3
+# 3 + 2 = 5
+print(listOfIntegers[0:6:2]) # 1, 3, 5
